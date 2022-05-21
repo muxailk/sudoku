5b6c08a282ce13e0ce27d244c9117efd9ad76ab2
muxailk <muxail1327@mail.ru>
2022-05-21 23:04:58 +0300

    added css priority

--- frontend/script.js
+++ frontend/script.js
@@ -1,25 +1,17 @@
-const table = document.querySelector('.container')
-const boxes = document.querySelectorAll('.box')
+const table = document.querySelector('.container');
+const boxes = document.querySelectorAll('.box');
 let selectedCell, selectedBox, selectedColumn, selectedRow
 
 
 table.addEventListener('click', function(event) {
     if (!event.target.classList.contains('cell')) return;
     
-    const { column, row } = event.target.dataset
+    const { column, row } = event.target.dataset;
 
     if (selectedCell && selectedBox){
         selectedCell.classList.remove('selected')
         selectedBox.classList.remove('active')
-    } 
-
-    // Paint selected cell
-    selectedCell = event.target
-    selectedCell.classList.add('selected')
-    
-    // Paint selected box
-    selectedBox = selectedCell.closest('.box')
-    selectedBox.classList.add('active');
+    };
 
     // Paint column and row
     [...boxes].forEach((box, index) => {
@@ -27,8 +19,16 @@ table.addEventListener('click', function(event) {
             const { column: childColumn, row: childRow } = child.dataset
 
             if (childColumn === column || childRow === row) {
-                child.classList.add('selected');
+                child.classList.add('active');
             }
         })
-    })
+    });
+
+    // Paint selected cell
+    selectedCell = event.target;
+    selectedCell.classList.add('selected');
+    
+    // Paint selected box
+    selectedBox = selectedCell.closest('.box');
+    selectedBox.classList.add('active');
 })
